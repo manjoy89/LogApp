e9132aaa37a77cfa5f5d6bcd30ecfb20b39f0e82
Manoj Kumar Mohan <manoj_mohan@genpt.com>
2019-01-18 14:31:16 -0500

    first commit

---
+++ readfile.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var cors = require('cors');
+
+app.use(cors());
+app.use('/', (req, res) => {
+
+    fs.readFile('/Users/pctr487330/Desktop/Newfile.txt', (err, data) => {
+        if (err) throw e;
+        res.send(data);
+    });
+});
+
+app.listen(5555);
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { splitTypescriptSuffix } from '@angular/compiler/src/aot/util';
+
+
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  Content:String;
+  Staging:String;
+
+
+  constructor(private http: HttpClient) { 
+    var myInterval = setInterval(() =>{
+    this.http.get('http://localhost:5555/',{responseType: 'text'}).subscribe(data=>{
+
+      this.Staging = data;
+      var textarea = document.getElementById('textarea_id');
+      textarea.style.backgroundColor = "";
+      textarea.scrollTop = textarea.scrollHeight;
+      this.Content = this.Staging.split('Error').join('<=============================Error==========================>');
+    })
+  }, 1000);
+   
+ 
+  }
+
+  ngOnInit() {
+
+  }
+/*
+  public onChange(fileList: FileList): void {
+    let file = fileList[0];
+    console.log(file);
+    let fileReader: FileReader = new FileReader();
+    let self = this;
+    console.log(file);
+    fileReader.onloadend = function(x) {
+      self.Content = fileReader.result as string;
+    }
+    var myInterval = setInterval(function() {
+    fileReader.readAsText(file);
+    var textarea = document.getElementById('textarea_id');
+    textarea.scrollTop = textarea.scrollHeight;
+  }, 1000);
+  }  
+  */
+
+ 
+
+}
