67dee6971a0a58e834db1a55885b02ee44e07f7d
Manoj Kumar Mohan <manoj_mohan@genpt.com>
2019-01-19 00:54:16 -0500

    Commit message13

--- readfile.js
+++ readfile.js
@@ -6,13 +6,11 @@ const path = require('path');
 
 app.use(cors());
 app.use('/', (req, res) => {
-    res.send("Hello Manoj");
-    console.log('Hello Manoj');
-
-  //  fs.readFile('/Users/pctr487330/Desktop/Newfile.txt', (err, data) => {
-    //    if (err) throw err;
-  //      res.send("Hello Manoj");
-  //  });
+   
+    fs.readFile('/Users/pctr487330/Desktop/Newfile.txt', (err, data) => {
+        if (err) throw err;
+        res.send("Hello Manoj");
+    });
 });
 
 app.listen(5000);
