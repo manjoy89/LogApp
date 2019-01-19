bcd2e39c8c0ca7e90fe25eaa910cfd9f239edc18
Manoj Kumar Mohan <manoj_mohan@genpt.com>
2019-01-18 19:55:44 -0500

    server.js fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/LogApp'));
+app.use(express.static(__dirname + '/dist/log-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/LogApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/log-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
